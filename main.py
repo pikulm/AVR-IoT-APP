bb459927e6833dd954f96b4a435978c73a338038
Magdalena Pikul <magdalena.anna.pikul@gmail.com>
2020-12-06 21:31:06 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ToggleButtonBehavior
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class HomeScreen(Screen):
+    pass
+
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+
+class ToggleButton(ToggleButtonBehavior, Image):
+    pass
+
+
+GUI = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return GUI
+
+    def change_screen(self, screen_name):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.current = screen_name
+
+MainApp().run()
